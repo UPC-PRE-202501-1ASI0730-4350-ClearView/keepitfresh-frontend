d4117415e47109cf5e2cf5cc23b743ca988654ea
marquinho04 <marcoanakasone@gmail.com>
2025-05-10 11:04:54 -0500

    feat(alerts): add alerts component.

---
+++ src/public/pages/alerts.components.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "alerts"
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>Alerts</h1>
+    <p>prueba router</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
