4d56f8cca3e837e8b297377bcc0b5c891d816145
Werner Lang <U202310003@upc.edu.pe>
2025-05-12 16:55:25 -0500

    feat(waiter-alert): add base component.

---
+++ src/public/pages/alerts/waiter-alerts.component.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "waiter-alerts"
+})
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>Waiter Alerts</h1>
+    <p>prueba router</p>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
