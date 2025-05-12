67039c2401a243af1077fcded6238f580aea08ab
Werner Lang <U202310003@upc.edu.pe>
2025-05-12 17:58:48 -0500

    fix: reverted some changes that made the frontend not work as intended.

--- src/public/pages/alerts/inventory-alerts.component.vue
+++ src/public/pages/alerts/inventory-alerts.component.vue
@@ -1,43 +1,8 @@
-<script setup>
-import {onMounted, ref} from "vue";
-import { Message as PvMessage } from "primevue";
-
-const notifications = ref([]);
-
-const fetchNotifications = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/inventory-notifications');
-    notifications.value = await response.json();
-  } catch (error) {
-    console.error('Error fetching notifications:', error);
-  }
-};
-
-
-onMounted(() => {
-  fetchNotifications();
-});
+<script>
 
 </script>
 
 <template>
-  <div class="w-full">
-    <h1>Inventory Alerts</h1>
-    <!-- Contenedor de notificaciones -->
-    <div class="message-container">
-      <!-- Notificaciones de Inventario -->
-        <pv-message
-            v-for="notification in notifications"
-            :key="notification.title"
-            closable
-            severity="error"
-            icon="pi pi-exclamation-circle"
-            @close ="notification.closed = true"
-        >
-          {{ notification.description }}
-        </pv-message>
-    </div>
-  </div>
 </template>
 
 <style scoped>
