1d0fa2c2af5c40a6b90a946fa0ca290d108392df
Werner Lang <U202310003@upc.edu.pe>
2025-05-12 17:37:22 -0500

    feat(inventory-alert): base logic for inventory alerts

--- src/public/pages/alerts/inventory-alerts.component.vue
+++ src/public/pages/alerts/inventory-alerts.component.vue
@@ -1,13 +1,42 @@
-<script>
-export default {
-  name: "inventory-alerts"
-}
+<script setup>
+import {onMounted, ref} from "vue";
+import { Message as PvMessage } from "primevue";
+
+const notifications = ref([]);
+
+const fetchNotifications = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/inventory-notification');
+    notifications.value = await response.json();
+  } catch (error) {
+    console.error('Error fetching notifications:', error);
+  }
+};
+
+
+onMounted(() => {
+  fetchNotifications();
+});
+
 </script>
 
 <template>
   <div class="w-full">
     <h1>Inventory Alerts</h1>
-    <p>prueba router</p>
+    <!-- Contenedor de notificaciones -->
+    <div class="message-container">
+      <!-- Notificaciones de Inventario -->
+        <pv-message
+            v-for="notification in notifications"
+            :key="notification.title"
+            closable
+            severity="error"
+            icon="pi pi-exclamation-circle"
+            @close ="notification.closed = true"
+        >
+          {{ notification.description }}
+        </pv-message>
+    </div>
   </div>
 </template>
 
