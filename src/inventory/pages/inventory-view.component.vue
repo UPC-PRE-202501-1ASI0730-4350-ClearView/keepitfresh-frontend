561a6a342a3a6de0664ab618c03b445a5dff450b
Pedro Guia <u202212010@upc.edu.pe>
2025-05-12 19:28:56 -0500

    feat(inventory): add page inventory.

---
+++ src/inventory/pages/inventory-view.component.vue
@@ -0,0 +1,15 @@
+<script setup>
+import InventoryForm from '../components/inventory-form.component.vue'
+import InventoryList from '../components/inventory-list.component.vue'
+
+defineOptions({
+  name: 'inventory-view'
+})
+</script>
+
+<template>
+  <div>
+    <InventoryForm />
+    <InventoryList />
+  </div>
+</template>
