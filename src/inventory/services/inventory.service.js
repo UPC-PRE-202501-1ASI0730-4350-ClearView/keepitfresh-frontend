8b327014be3bca7bd309217b7e9f28ebfbdb6dce
Pedro Guia <u202212010@upc.edu.pe>
2025-05-14 00:38:00 -0500

    fix: fixed service.

--- src/inventory/services/inventory.service.js
+++ src/inventory/services/inventory.service.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
+import Inventory from '../models/inventory.entity.js'; // tu clase Inventory
+
 const API_URL = '/api/inventory';
 
 export default {
-    getAll() {
-        return axios.get(API_URL).then(res => res.data);
+    async getAll() {
+        const response = await axios.get(API_URL);
+        return response.data.map(item => new Inventory(item));
     },
-    create(item) {
-        return axios.post(API_URL, item).then(res => res.data);
-    },
+
+    async create(item) {
+        const newItem = new Inventory(item);
+        const response = await axios.post(API_URL, newItem);
+        return new Inventory(response.data);
+    }
 };
